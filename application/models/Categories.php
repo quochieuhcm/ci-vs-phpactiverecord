ae254626a020cbf22cc95cc1edce9cb53a35e9ea
quochieuhcm <quochieuhcm@gmail.com>
2016-12-27 23:46:51 +0700

    Revert "Demo PHP ACTIVE RECORD"
    This reverts commit ce4b5faa05f7d1d02c72da16937d3f494475d1d3.

--- application/models/Categories.php
+++
@@ -1,7 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-class Categories extends ActiveRecord\Model {}
-
-/* End of file Categories.php */
-/* Location: ./application/models/Categories.php */
