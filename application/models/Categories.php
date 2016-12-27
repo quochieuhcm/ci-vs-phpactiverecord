ce4b5faa05f7d1d02c72da16937d3f494475d1d3
quochieuhcm <quochieuhcm@gmail.com>
2016-12-27 23:44:30 +0700

    Demo PHP ACTIVE RECORD
    Demo PHP ACTIVE RECORD

---
+++ application/models/Categories.php
@@ -0,0 +1,7 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Categories extends ActiveRecord\Model {}
+
+/* End of file Categories.php */
+/* Location: ./application/models/Categories.php */
